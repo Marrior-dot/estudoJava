de1220aa7aa7fdb1555c393e8f2c727dd78bfb62
Marrior-dot <59967704+Marrior-dot@users.noreply.github.com>
2023-09-04 10:40:12 -0300

    Delete codeWarsJava directory

--- codeWarsJava/crawlUp/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String args[]) {
-        Kata first = new Kata(5, 10,3);
-        System.out.println(first.snail());
-        //first.snail();
-
-    }
-}
