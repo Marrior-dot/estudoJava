e76bb6f4fc5c861745aee5341a7e80fe391ff2e0
Marrior <mrlimaroberto@gmail.com>
2023-11-22 22:51:10 -0300

    continuo amanhã krl quero dormir porra...

---
+++ src/main/java/taco/cloud/DesignTacoController.java
@@ -0,0 +1,35 @@
+package taco.cloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import taco.cloud.Ingredient;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("TacoOrder")
+public class DesignTacoController {
+
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        );
+    };
+
+    Ingredient.Type[] types = Ingredient.Type.values();//Array com os valores do Enum Type em Ingredient
+    
+
+}
