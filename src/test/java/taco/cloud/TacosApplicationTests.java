974205ea2d16f58813fd162d26659e906c470167
Marrior <mrlimaroberto@gmail.com>
2023-11-22 21:51:20 -0300

    primeiro commit

---
+++ src/test/java/taco/cloud/TacosApplicationTests.java
@@ -0,0 +1,13 @@
+package taco.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
